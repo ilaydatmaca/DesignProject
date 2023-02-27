62e5b263703d188594b89c29772dba8757ff8271
ilayda atmaca <atmacailyada@gmail.com>
2023-02-27 10:22:25 +0300

    Singleton

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    static T m_instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (m_instance == null)
+            {
+                m_instance = GameObject.FindObjectOfType<T>();
+
+                if (m_instance == null)
+                {
+                    GameObject singleton = new GameObject(typeof(T).Name);
+                    m_instance = singleton.AddComponent<T>();
+                }
+            }
+
+            return m_instance;
+        }
+    }
+
+    public void Awake()
+    {
+        if (m_instance == null)
+        {
+            m_instance = this as T;
+            transform.parent = null;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
