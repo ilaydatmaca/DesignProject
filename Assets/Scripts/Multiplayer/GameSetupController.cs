a1aeb93039e961a0a396309983684386bc3a65e2
ilayda atmaca <atmacailyada@gmail.com>
2023-03-29 20:08:54 +0300

    photon

--- Assets/Scripts/Multiplayer/GameSetupController.cs
+++ Assets/Scripts/Multiplayer/GameSetupController.cs
@@ -14,6 +14,6 @@ public class GameSetupController : MonoBehaviour
     private void CreatePlayer()
     {
         Debug.Log("Creating Player");
-        //PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PhotonPlayer"), Vector3.zero, Quaternion.identity);
+        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PhotonPlayer"), Vector3.zero, Quaternion.identity);
     }
 }
