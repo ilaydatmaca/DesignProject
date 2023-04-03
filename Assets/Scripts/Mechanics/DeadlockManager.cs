bb222de071a6348652656894d87f6e964e396a76
AtmacaSevval <sevvallatmaca@gmail.com>
2023-04-03 18:42:21 +0300

    shuffle bug

--- Assets/Scripts/Mechanics/DeadlockManager.cs
+++ Assets/Scripts/Mechanics/DeadlockManager.cs
@@ -15,7 +15,7 @@ public class DeadlockManager : MonoBehaviour
 
     }
     
-    List<GamePiece> GetRowOrColumnList(int x, int y, bool checkRow = true)
+    /*List<GamePiece> GetRowOrColumnList(int x, int y, bool checkRow = true)
     {
         List<GamePiece> piecesList = new List<GamePiece>();
 
@@ -134,5 +134,128 @@ public class DeadlockManager : MonoBehaviour
             }
         }
         return true;
+    }*/
+    
+     List<GamePiece> GetRowOrColumnList(GamePiece[,] allPieces, int x, int y, int listLength = 3, bool checkRow = true)
+    {
+
+        List<GamePiece> piecesList = new List<GamePiece>();
+
+        for (int i = 0; i < listLength; i++)
+        {
+            if (checkRow)
+            {
+                if (_board.IsInBorder(x + i, y) && allPieces[x + i, y] != null)
+                {
+                    piecesList.Add(allPieces[x + i, y]);
+                }
+            }
+            else
+            {
+                if (_board.IsInBorder(x, y + i) && allPieces[x, y + i] != null)
+                {
+                    piecesList.Add(allPieces[x, y + i]);
+                }
+            }
+        }
+        return piecesList;
+    }
+
+    List<GamePiece> GetMinimumMatches(List<GamePiece> gamePieces, int minForMatch = 2)
+    {
+        List<GamePiece> matches = new List<GamePiece>();
+
+        var groups = gamePieces.GroupBy(n => n.matchValue);
+
+        foreach (var grp in groups)
+        {
+            if (grp.Count() >= minForMatch && grp.Key != MatchValue.None)
+            {
+                matches = grp.ToList();
+            }
+        }
+        return matches;
+    }
+
+    List<GamePiece> GetNeighbors(GamePiece[,] allPieces, int x, int y)
+    {
+
+        List<GamePiece> neighbors = new List<GamePiece>();
+
+        Vector2[] searchDirections = new Vector2[4]
+        {
+            new Vector2(-1, 0),
+            new Vector2(1, 0),
+            new Vector2(0, 1),
+            new Vector2(0, -1)
+        };
+
+        foreach (Vector2 dir in searchDirections)
+        {
+            if (_board.IsInBorder(x + (int)dir.x, y + (int)dir.y))
+            {
+                if (allPieces[x + (int)dir.x, y + (int)dir.y] != null)
+                {
+                    if (!neighbors.Contains(allPieces[x + (int)dir.x, y + (int)dir.y]))
+                    {
+                        neighbors.Add(allPieces[x + (int)dir.x, y + (int)dir.y]);
+                    }
+                }
+            }
+        }
+        return neighbors;
+    }
+
+
+    bool HasMoveAt(GamePiece[,] allPieces, int x, int y, int listLength = 3, bool checkRow = true)
+    {
+        List<GamePiece> pieces = GetRowOrColumnList(allPieces, x, y, listLength, checkRow);
+
+        List<GamePiece> matches = GetMinimumMatches(pieces, listLength - 1);
+
+        GamePiece unmatchedPiece = null;
+        if (pieces != null && matches != null)
+        {
+            if (pieces.Count == listLength && matches.Count == listLength - 1)
+            {
+                unmatchedPiece = pieces.Except(matches).FirstOrDefault();
+            }
+
+            if (unmatchedPiece != null)
+            {
+                List<GamePiece> neighbors = GetNeighbors(allPieces, unmatchedPiece.xIndex, unmatchedPiece.yIndex);
+                neighbors = neighbors.Except(matches).ToList();
+                neighbors = neighbors.FindAll(n => n.matchValue == matches[0].matchValue);
+                matches = matches.Union(neighbors).ToList();
+            }
+            
+            if (matches.Count >= listLength)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public bool IsDeadlocked()
+    {
+        int listLength = 3;
+        int width = _board.AllGamePieces.GetLength(0);
+        int height = _board.AllGamePieces.GetLength(1);
+
+        bool isDeadlocked = true;
+
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                if (HasMoveAt(_board.AllGamePieces, i, j, listLength, true) || HasMoveAt(_board.AllGamePieces, i, j, listLength, false))
+                {
+                    isDeadlocked = false;
+
+                }
+            }
+        }
+        return isDeadlocked;
     }
 }
