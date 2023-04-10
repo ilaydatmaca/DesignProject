fdbb91abaada31370f1179288923bcadda96961a
AtmacaSevval <sevvallatmaca@gmail.com>
2023-04-10 10:27:32 +0300

    rename

--- Assets/Scripts/UI/SmoothMovePopup.cs
+++ Assets/Scripts/UI/SmoothMovePopup.cs
@@ -19,7 +19,7 @@ public class SmoothMovePopup : MonoBehaviour
 
     IEnumerator SmoothTranslation( float speed)
     {
-        Vector3 target = new Vector3(_rectTransform.position.x, 250, _rectTransform.position.z);
+        Vector3 target = new Vector3(_rectTransform.position.x, 210, _rectTransform.position.z);
 
         while (_rectTransform.position.y - target.y >= 0.2f) {
             _rectTransform.position = Vector3.Lerp (_rectTransform.position, target, Time.deltaTime * speed);
