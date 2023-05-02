08a540f6ebb32e5db8c8738c51d1a7b22c41b982
ilayda atmaca <atmacailyada@gmail.com>
2023-05-02 11:11:24 +0300

    playfab login

---
+++ Assets/Scripts/Playfab/PlayFabLogin.cs
@@ -0,0 +1,34 @@
+using System;
+using Photon.Realtime;
+using PlayFab;
+using PlayFab.ClientModels;
+using UnityEngine;
+
+public class PlayFabLogin : MonoBehaviour
+{
+    void Start()
+    {
+        Login();
+    }
+
+    void Login()
+    {
+        var request = new LoginWithCustomIDRequest
+        {
+            CustomId = SystemInfo.deviceUniqueIdentifier,
+            CreateAccount = true
+        };
+        PlayFabClientAPI.LoginWithCustomID(request, OnSuccess, OnError);
+    }
+
+    void OnSuccess(LoginResult result)
+    {
+        Debug.Log("Successfull login/account create!");
+    }
+
+    void OnError(PlayFabError error)
+    {
+        Debug.Log("Error while logging in/creating account!");
+        Debug.Log(error.GenerateErrorReport());
+    }
+}
