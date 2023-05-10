7e443c3884f64418e7a69932af3fad6aeefdc85b
AtmacaSevval <sevvallatmaca@gmail.com>
2023-05-10 20:17:35 +0300

    new assets

--- Assets/Scripts/Particle/ParticleManager.cs
+++ Assets/Scripts/Particle/ParticleManager.cs
@@ -4,6 +4,7 @@ public class ParticleManager : MonoBehaviour
 {
 	public GameObject clearFXPrefab;
 	public GameObject bombFXPrefab;
+	public GameObject boosterFXPrefab;
 
 	public void ClearPieceFXAt(int x, int y, int z = 0)
 	{
@@ -23,6 +24,20 @@ public class ParticleManager : MonoBehaviour
 	public void BombFXAt(int x, int y, int z = 0)
 	{
 		if (bombFXPrefab !=null)
+		{
+			GameObject bombFX = Instantiate(bombFXPrefab, new Vector3(x, y, z), Quaternion.identity) as GameObject;
+			ParticlePlayer particlePlayer = bombFX.GetComponent<ParticlePlayer>();
+
+			if (particlePlayer !=null)
+			{
+				particlePlayer.Play();
+			}
+		}
+	}
+	
+	public void BoosterFXAt(int x, int y, int z = 0)
+	{
+		if (boosterFXPrefab !=null)
 		{
 			GameObject bombFX = Instantiate(bombFXPrefab, new Vector3(x, y, z), Quaternion.identity) as GameObject;
 			ParticlePlayer particlePlayer = bombFX.GetComponent<ParticlePlayer>();
